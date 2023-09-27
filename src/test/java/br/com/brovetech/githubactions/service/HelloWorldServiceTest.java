d38005b056070c7ce6aa9ccdce9182c402c62089
Naidion Brovedan <brovedan@gmail.com>
2023-09-28 00:05:32 +0200

    Configured tests

---
+++ src/test/java/br/com/brovetech/githubactions/service/HelloWorldServiceTest.java
@@ -0,0 +1,21 @@
+package br.com.brovetech.githubactions.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldServiceTest {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    @Test
+    void sumNumbers() {
+        Integer sum = helloWorldService.sumNumbers(1,2,3);
+        Assertions.assertEquals(6, 6);
+    }
+}
