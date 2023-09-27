a301ec58fc9e48f6b200d948f14194dd9f3bd55c
Naidion Brovedan <brovedan@gmail.com>
2023-09-28 00:25:42 +0200

    Created more tests

--- src/test/java/br/com/brovetech/githubactions/service/HelloWorldServiceTest.java
+++ src/test/java/br/com/brovetech/githubactions/service/HelloWorldServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,8 +16,33 @@ class HelloWorldServiceTest {
     private HelloWorldService helloWorldService;
 
     @Test
-    void sumNumbers() {
-        Integer sum = helloWorldService.sumNumbers(1,2,3);
-        Assertions.assertEquals(6, sum);
+    void testSumNumbers() {
+        Integer result = helloWorldService.sumNumbers(1,2,3);
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
+    void maxNumber() {
+        Integer result = helloWorldService.maxNumber(1,2,3);
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    void minNumber() {
+        Integer result = helloWorldService.minNumber(1,2,3);
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void average() {
+        Double result = helloWorldService.average(1,2,3);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void getHelloWorld() {
+        Map<String, String> result = helloWorldService.getHelloWorld();
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("world!!!", result.get("hello"));
     }
 }
